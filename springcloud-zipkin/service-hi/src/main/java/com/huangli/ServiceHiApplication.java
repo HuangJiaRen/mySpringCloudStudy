e846b2da5ce7ee144dd1cac0dc93074dc719e8f9
huangli <1466384630@qq.com>
2019-07-02 18:18:56 +0800

    增加eureka配置

--- springcloud-zipkin/service-hi/src/main/java/com/huangli/ServiceHiApplication.java
+++ springcloud-zipkin/service-hi/src/main/java/com/huangli/ServiceHiApplication.java
@@ -4,10 +4,12 @@ import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +20,7 @@ import java.util.logging.Logger;
  */
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ServiceHiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
--- springcloud-zipkin/service-miya/src/main/java/com/huangli/cn/ServiceMiyaApplication.java
+++ springcloud-zipkin/service-miya/src/main/java/com/huangli/cn/ServiceMiyaApplication.java
@@ -4,6 +4,7 @@ import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ServiceMiyaApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceMiyaApplication.class, args);
